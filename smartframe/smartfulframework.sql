e38d6cd48621fe78e2f04d35cae16ab580687b35
smartful <remi_rodrigues@hotmail.com>
2016-11-08 00:32:42 +0100

    mise à jour de l'échantillon de base de la BDD

--- smartframe/smartfulframework.sql
+++ smartframe/smartfulframework.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4
+-- version 4.5.2
 -- http://www.phpmyadmin.net
 --
--- Client: localhost
--- Généré le: Lun 25 Août 2014 à 23:55
--- Version du serveur: 5.6.12-log
--- Version de PHP: 5.4.16
+-- Client :  localhost
+-- Généré le :  Mar 08 Novembre 2016 à 00:29
+-- Version du serveur :  10.1.16-MariaDB
+-- Version de PHP :  7.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,13 +14,11 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données: `aupresserre`
+-- Base de données :  `smartfulframework`
 --
-CREATE DATABASE IF NOT EXISTS `smartfulframework` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `smartfulframework`;
 
 -- --------------------------------------------------------
 
@@ -28,8 +26,8 @@ USE `smartfulframework`;
 -- Structure de la table `adherents`
 --
 
-CREATE TABLE IF NOT EXISTS `adherents` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `adherents` (
+  `id` int(11) NOT NULL,
   `prenom` varchar(100) NOT NULL,
   `nom` varchar(100) NOT NULL,
   `email` varchar(255) NOT NULL,
@@ -37,16 +35,16 @@ CREATE TABLE IF NOT EXISTS `adherents` (
   `sexe` enum('f','m') NOT NULL,
   `age` int(11) NOT NULL,
   `date_inscription` datetime NOT NULL,
-  `validation` int(4) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+  `validation` int(4) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `adherents`
 --
 
 INSERT INTO `adherents` (`id`, `prenom`, `nom`, `email`, `pass`, `sexe`, `age`, `date_inscription`, `validation`) VALUES
-(7, 'grande', 'MADAME', 'grande_madame@hotmail.com', 'caa70946d8da3b59d1e0e798712934907f004695', 'f', 23, '2014-06-23 17:08:04', 1);
+(8, 'Petit', 'Monsieur', 'petit_monsieur@gmail.com', '7c222fb2927d828af22f592134e8932480637c0d', 'm', 26, '2016-11-07 00:33:58', 1),
+(9, 'Grande', 'Madame', 'grande_madame@hotmail.com', 'caa70946d8da3b59d1e0e798712934907f004695', 'f', 28, '2016-11-07 23:23:36', 1);
 
 -- --------------------------------------------------------
 
@@ -54,12 +52,11 @@ INSERT INTO `adherents` (`id`, `prenom`, `nom`, `email`, `pass`, `sexe`, `age`, 
 -- Structure de la table `admin`
 --
 
-CREATE TABLE IF NOT EXISTS `admin` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
   `nom_admin` varchar(50) NOT NULL,
-  `pass_admin` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `pass_admin` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `admin`
@@ -74,12 +71,11 @@ INSERT INTO `admin` (`id`, `nom_admin`, `pass_admin`) VALUES
 -- Structure de la table `albums`
 --
 
-CREATE TABLE IF NOT EXISTS `albums` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `albums` (
+  `id` int(11) NOT NULL,
   `titre` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `albums`
@@ -88,7 +84,8 @@ CREATE TABLE IF NOT EXISTS `albums` (
 INSERT INTO `albums` (`id`, `titre`, `description`) VALUES
 (1, 'test_1', 'Ceci est un album de test. \r\nTest numéro 1 !										\r\n										'),
 (2, 'test_2', 'Et voilà le TEEEEEEEEEEEEEEEst 2 !!!\r\nSamba !!!										\r\n										'),
-(4, 'test_3', 'Et pour le fun, un troisième test !!!										\r\n										');
+(4, 'test_3', 'Et pour le fun, un troisième test !!!										\r\n										'),
+(8, 'test_4', 'Eh mais Ouaaaaaaaaaais !!!!!!											');
 
 -- --------------------------------------------------------
 
@@ -96,19 +93,18 @@ INSERT INTO `albums` (`id`, `titre`, `description`) VALUES
 -- Structure de la table `archives`
 --
 
-CREATE TABLE IF NOT EXISTS `archives` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `archives` (
+  `id` int(11) NOT NULL,
   `titre` varchar(500) NOT NULL,
-  `date_ajout` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  `date_ajout` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `archives`
 --
 
 INSERT INTO `archives` (`id`, `titre`, `date_ajout`) VALUES
-(3, 'Note-robotique-Rivaton-2', '2014-06-24 22:44:13');
+(5, 'Initiez-vous au design - activité', '2016-11-08 00:02:38');
 
 -- --------------------------------------------------------
 
@@ -116,22 +112,22 @@ INSERT INTO `archives` (`id`, `titre`, `date_ajout`) VALUES
 -- Structure de la table `billets`
 --
 
-CREATE TABLE IF NOT EXISTS `billets` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `billets` (
+  `id` int(11) NOT NULL,
   `titre` varchar(255) NOT NULL,
   `contenu` text NOT NULL,
   `date_ajout` datetime NOT NULL,
-  `date_modif` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+  `date_modif` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `billets`
 --
 
 INSERT INTO `billets` (`id`, `titre`, `contenu`, `date_ajout`, `date_modif`) VALUES
-(1, 'Sortie Kart', 'L’Association Machin veut s\'amuser. Séance de Karting programmé pour le samedi 15 octobre 2016.', '2014-06-23 18:46:33', '2014-06-23 18:46:33'),
-(4, 'La semaine de la science ', 'Du 8 au 16 octobre c\'est la fête de la science, et nous comptons bien y apporter notre dose de folie ! \r\nVenez nous tenir main forte !!!', '2014-06-23 18:54:59', '2014-06-23 19:15:41');
+(1, 'Sortie Kart', 'L’Association Machin veut s''amuser. Séance de Karting programmé pour le  15 décembre 2016.											', '2014-06-23 18:46:33', '2016-11-07 02:19:03'),
+(4, 'La semaine de la science ', 'Du 8 au 16 octobre c''est la fête de la science, et nous comptons bien y apporter notre dose de folie ! \r\nVenez nous tenir main forte !!!', '2014-06-23 18:54:59', '2014-06-23 19:15:41'),
+(5, 'Nounours Party', 'Nounours organise une fête de folie ! \r\nFerez vous parti de l''aventure ?																							', '2016-11-07 02:18:32', '2016-11-07 02:18:32');
 
 -- --------------------------------------------------------
 
@@ -139,12 +135,11 @@ INSERT INTO `billets` (`id`, `titre`, `contenu`, `date_ajout`, `date_modif`) VAL
 -- Structure de la table `photos`
 --
 
-CREATE TABLE IF NOT EXISTS `photos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `photos` (
+  `id` int(11) NOT NULL,
   `id_album` int(11) NOT NULL,
-  `titre` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
+  `titre` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `photos`
@@ -154,8 +149,83 @@ INSERT INTO `photos` (`id`, `id_album`, `titre`) VALUES
 (16, 2, 'espace_lune.jpg'),
 (18, 1, 'nage.jpg'),
 (24, 4, 'ciel_vaisseau.jpeg'),
-(25, 4, 'ciel_vaisseau2.jpg');
+(25, 4, 'ciel_vaisseau2.jpg'),
+(28, 8, 'DSC_0018.JPG');
 
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `adherents`
+--
+ALTER TABLE `adherents`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `albums`
+--
+ALTER TABLE `albums`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `archives`
+--
+ALTER TABLE `archives`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `billets`
+--
+ALTER TABLE `billets`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `photos`
+--
+ALTER TABLE `photos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `adherents`
+--
+ALTER TABLE `adherents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT pour la table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `albums`
+--
+ALTER TABLE `albums`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT pour la table `archives`
+--
+ALTER TABLE `archives`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT pour la table `billets`
+--
+ALTER TABLE `billets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT pour la table `photos`
+--
+ALTER TABLE `photos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
